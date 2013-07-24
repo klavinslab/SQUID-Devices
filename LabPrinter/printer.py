a998f8b0dcbb67f5321dbd90645630232ee35de7
JGSuw <jgs6156@uw.edu>
2013-07-24 14:20:41 -0700

    Seriously the first commit of LabPrinter

---
+++ LabPrinter/printer.py
@@ -0,0 +1,134 @@
+#!/usr/bin/env python\
+import cups
+import json
+import basedevice
+import time
+import thread
+
+"""
+Programmer: Joseph Sullivan
+Research Assistant, ISTC PC
+
+PrintRequestHandler and Labprinter are classes which subclass BaseDevice and BaseDeviceRequestHandler
+the device has two possible commands: info and print. Print in turn has three possible fields: 'text', 'barcode'
+and 'copies'. Barcode has yet to be implemented.
+
+TODO: in next version create methods that format the document which will be printed so that text wraps properly."""
+
+
+class PrinterRequestHandler(basedevice.BaseDeviceRequestHandler):
+    #code
+    
+    def do_cmd_print(self):
+        
+        cups = self.state['cups']
+        query = self.query
+        
+        #This logic block is included for barcode implementation in the future, if barcodes are desired.
+        if query.has_key('text') & query.has_key('barcode'):
+            self.makeFile(query.get('text'),query.get('barcode'))
+            if query.has_key('copies'):
+                for i in range(0,int(query.get('copies'))):
+                    cups.printFile(cups.getDefault(),'temp_print_file', str(i),{})
+                    
+                self.send_response(200)
+                self.send_header("content-type", "text/plain")
+                self.end_headers() 
+                    
+            else:
+                cups.printFile(cups.getDefault(),'temp_print_file','job',{})
+          
+        #Primary code block that prints a text file        
+        else:
+            if query.has_key('text'):
+                self.makeFile(query.get('text'),'null')
+                if query.has_key('copies'):
+                    for i in range(0,int(query.get('copies'))):
+                        cups.printFile(cups.getDefault(),'temp_print_file', str(i),{})
+                        
+                    self.send_response(200)
+                    self.send_header("content-type", "text/plain")
+                    self.end_headers()
+                        
+                else:
+                    cups.printFile(cups.getDefault(),'temp_print_file','job',{})
+                    
+                    self.send_response(200)
+                    self.send_header("content-type", "text/plain")
+                    self.end_headers()
+                
+            else :
+            #No text given, bad request.
+                self.send_response(400) #bad request
+                self.send_header("content-type", "text/plain")
+                self.end_headers()
+                self.wfile.write("Cannot print without text")
+                pass
+        pass
+    
+    
+    def do_cmd_info(self):
+        self.send_response( 200 )
+        self.send_header("content-type", "text/plain")
+        self.end_headers()
+        info = {"uuid" : self.state["uuid"],
+          "status" : self.state["connection"],
+          "state" : "",
+          "name" : "Zebra GK420t printer"}
+        if self.state['connection'] == 'not_connected':
+            info.update({"status":"not ready - no CUPS connection"})
+        response = json.dumps(info)
+        self.wfile.write(response)
+        pass
+    
+    #This makes the file that is sent to the CUPS server
+    def makeFile(self, text, barcode):
+        if barcode == 'null':
+            outfile = open('temp_print_file', 'w')
+            outfile.write('\n' + text + '\n')
+            
+        else:
+            #outfile = open('/temp/temp_print_file','w')
+            #NEED TO FIGURE OUT HOW TO PRINT BYTES IN PYTHON,
+            #CAN GET BYTES FROM BARCODE LIBRARY
+            outfile = open('temp_print_file', 'w')
+            outfile.write('\n')
+            outfile.write(text + '\n')
+        pass    
+    
+class labprinter(basedevice.BaseDevice):
+    
+    def __init__(self, handler):
+        basedevice.BaseDevice.__init__(self, handler)
+        self.state['connection'] = 'not_connected'
+        while self.state['connection'] != 'connected':
+            try:
+                self.state['cups'] = cups.Connection()
+                self.state['connection'] = 'connected'
+            except Exception:
+                print 'Initialization failed: unable to establish CUPS connection.'
+                print Exception.__doc__
+                time.sleep(5)
+                pass
+        
+        
+    def work(self):
+        while 1:
+            #What should we do?
+            #How about we check to see that there are still printers connected?
+            time.sleep(5)
+        pass
+    
+    
+    
+    
+    
+#program entry point
+if __name__ == '__main__':
+   PORT = 8000
+   dev = labprinter(PrinterRequestHandler)
+   dev.start()
+   print "serving at port", PORT
+   raw_input ("type something to stop: ")
+   dev.stop()#!/usr/bin/env python
+
